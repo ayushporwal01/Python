e6e1de2bf6a5b8fa25959aba0e9a03c98a6667cb
ayushporwal01 <porwalayush953@gmail.com>
2026-03-06 22:55:02 +0530

    Initial Commit

--- Basics/operators.py
+++ Basics/operators.py
@@ -12,3 +12,24 @@ print("num1 ** num2 is", num1 ** num2) #power
 
 #Arithmetic Operators
 a = 4
+print(a) #4
+
+a += 4
+print(a) #8
+
+a -= 4  
+print(a) #4
+
+a *= 4
+print(a) #16
+
+a /= 4
+print(a) #4.0
+
+a //= 4
+print(a) #1.0 
+
+a **= 4
+print(a) #1.0
+
+
