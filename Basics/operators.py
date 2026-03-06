472a5b487de2ef7e8a46a71f2a396e9e90763b09
ayushporwal01 <porwalayush953@gmail.com>
2026-03-06 22:47:46 +0530

    Initial Commit

--- Basics/operators.py
+++ Basics/operators.py
@@ -2,8 +2,13 @@ num1 = 10
 num2 = 2
 
 #Arithmetic Operators
-print("num1 + num2 is ", num1 + num2)
-print("num1 - num2 is ", num1 - num2)
-print("num1 * num2 is ", num1 * num2)
-print("num1 / num2 is ", num1 / num2)
-print("num1 % num2 is ", num1 % num2)
+print("num1 + num2 is", num1 + num2)
+print("num1 - num2 is", num1 - num2)
+print("num1 * num2 is", num1 * num2)
+print("num1 / num2 is", num1 / num2)
+print("num1 % num2 is", num1 % num2)
+print("num1 // num2 is", num1 // num2) #floor 
+print("num1 ** num2 is", num1 ** num2) #power
+
+#Arithmetic Operators
+a = 4
