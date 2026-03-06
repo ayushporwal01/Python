0131ed3241ac6e2904fa363c2be7e0b15d3d5de7
ayushporwal01 <porwalayush953@gmail.com>
2026-03-06 22:36:25 +0530

    Initial Commit

--- basics.py
+++ basics.py
@@ -1,3 +1,21 @@
 print("Hello")
 
 #Variables & Data Types
+
+a = 5
+print(a) #5
+
+b = 55
+print(b) #55
+
+a = 6.2
+print(a) #6.2
+
+c = True
+print(c) #True
+
+d = "Ayush"
+print(d) #Ayush
+
+e = None
+print(e)
