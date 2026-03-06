b64864430bd71577f55916394a59e7fc77e6993d
ayushporwal01 <porwalayush953@gmail.com>
2026-03-06 22:44:34 +0530

    Initial Commit

--- Basics/operators.py
+++ Basics/operators.py
@@ -3,3 +3,7 @@ num2 = 2
 
 #Operators
 print("num1 + num2 is ", num1 + num2)
+print("num1 - num2 is ", num1 - num2)
+print("num1 * num2 is ", num1 * num2)
+print("num1 / num2 is ", num1 / num2)
+print("num1 % num2 is ", num1 % num2)
