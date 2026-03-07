cc6cf72f4d2c05c55a84c56c06eca023998b3973
ayushporwal01 <porwalayush953@gmail.com>
2026-03-07 22:10:36 +0530

    Initial commit

--- Basics/08_sets.py
+++ Basics/08_sets.py
@@ -4,14 +4,14 @@
 a1 = {3, 5, 23, 45, 69, 78}
 a2 = {3, 5, 23, 7, 100}
 
-#a1.clear()                   #set()
-#a1.pop()                     #removes random element
-#a1.remove(43)                #error raised when given element not found
-#a1.discard(45)               #no error raised
+#a1.clear()                      #set()
+#a1.pop()                        #removes random element
+#a1.remove(43)                   #error raised when given element not found
+#a1.discard(45)                  #no error raised
 
-#res = a1.difference(a2)      #elements that are present in a1 but not in a2.
+#res = a1.difference(a2)         #elements that are present in a1 but not in a2.
 
-#a1.add(7)                    #adds given ele in a random place if it's not already present
+#a1.add(7)                       #adds given ele in a random place if it's not already present
  
 #a3 = a1.copy()     
 #a4 = a1.union(a2);            
@@ -22,5 +22,5 @@ a2 = {3, 5, 23, 7, 100}
 #a5 = a1.intersection(a2)
 #print(a5);                   
           
-#a1.update(a2);               #copies all elements of a2 in a1 
-#print(a2);                   #remains same
+#a1.update(a2);                 #copies all elements of a2 in a1 
+#print(a2);                     #remains same
