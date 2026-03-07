71890241f5921b6a5df8eff33c981b61668acdd4
ayushporwal01 <porwalayush953@gmail.com>
2026-03-07 16:16:30 +0530

    Initial Commit

--- Basics/06 - list.py
+++ Basics/06 - list.py
@@ -1,9 +1,18 @@
-l1 = [1, 2, 3, 3, "Ayush", 4, 5]
+l1 = [1, 4, 3, 3, "Ayush", 2, 5]
 
 print(type(l1)) #<class 'list'>
-print(l1) #1 2 3 3 'Ayush' 4 5 
+print(l1) #1 4 3 3 'Ayush' 2 5 
 
 l1.remove("Ayush")
-print(l1) #1 2 3 3 4 5
+print(l1) #1 4 3 3 2 5
 
 print(l1.count(3)) #2
+
+l1.sort()
+print(l1) #1 2 3 3 4 5
+
+l1.pop()
+print(l1) #1 2 3 3 4
+
+l1.append(10)
+print(l1) #1 2 3 3 4 10
