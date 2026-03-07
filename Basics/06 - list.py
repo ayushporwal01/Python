5faa4236db346bad777fb0df344e659029440cbd
ayushporwal01 <porwalayush953@gmail.com>
2026-03-07 16:10:02 +0530

    Initial Commit

--- Basics/06 - list.py
+++ Basics/06 - list.py
@@ -1,7 +1,10 @@
-l1 = [1, 2, 3, "Ayush", 4, 5]
+l1 = [1, 2, 3, 3, "Ayush", 4, 5]
 
 print(type(l1))
 print(l1)
 
 l1.remove("Ayush")
-print(l1) #1 2 3 4 5
+print(l1) #1 2 3 3 4 5
+
+l1.count(3)
+print(l1) #2
