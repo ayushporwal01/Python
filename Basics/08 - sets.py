431ffb0b9c404c738ec99060b3eb79c370d93720
ayushporwal01 <porwalayush953@gmail.com>
2026-03-07 16:43:52 +0530

    Initial Commit

--- Basics/08 - sets.py
+++ Basics/08 - sets.py
@@ -5,5 +5,6 @@ a2 = {3, 5, 23}
 
 #a1.clear() #set()
 #a1.pop() #removes random element
-#a1.remove(23) #3 5 45 69 78
+#a1.remove(23) #error raised when given element no found
+a1.discard(45) #no error raised
 print(a1)
