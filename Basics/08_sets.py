168ac018bc1bcf6bfea50bea9baa13dbc1699801
ayushporwal01 <porwalayush953@gmail.com>
2026-03-07 21:31:20 +0530

    Initial commit

--- Basics/08_sets.py
+++ Basics/08_sets.py
@@ -20,7 +20,7 @@ a2 = {3, 5, 23, 7, 100}
 #print(a2.issubset(a1));                 
 #print(a1.issuperset(a2));       
           
-a1.update(a2);           
-print(a1);                      
+a1.update(a2);                #copies all elements of a2 in a1 
+print(a1);                    
 print(a2);                    
 
