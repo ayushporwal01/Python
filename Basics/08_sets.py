59674ebd5aca8489a5586f2c8f7c61c78ba05cd4
ayushporwal01 <porwalayush953@gmail.com>
2026-03-07 22:11:58 +0530

    Initial commit

--- Basics/08_sets.py
+++ Basics/08_sets.py
@@ -20,6 +20,6 @@ a2 = {3, 5, 23, 7, 100}
 #print(a1.issuperset(a2));       
 #print(a1.intersection(a2))               
           
-#a1.update(a2);                 #copies all elements of a2 in a1 
-#print(a1);                     #a1 + a2
-#print(a2);                     #remains same
+#a1.update(a2);                  #copies all elements of a2 in a1 
+#print(a1);                      #a1 + a2
+#print(a2);                      #remains same
