da18ff6b7eccac14ef23810fe19747f4fbb004ff
ayushporwal01 <porwalayush953@gmail.com>
2026-03-07 11:18:45 +0530

    Initial Commit

--- Basics/04_strings.py
+++ Basics/04_strings.py
@@ -1,4 +1,6 @@
 name = 'Ayush'
+number = "7"
+
 print(name)
 
 print(name[0:3]) #Ayu
@@ -9,4 +11,6 @@ print(name.endswith("h")) #True
 print(name.find("h")) #4
 print(name.center(15, "*")) #*****Ayush*****
 print(name.isalnum()) #True
-print(name.isalpha()) #True
+print(number.isalpha()) #False
+print(number.isnumeric()) #True
+print(number.isdigit()) #True
