7ce6d6fe8c60c25be1bdc671db1fb35c1d74ff7c
ayushporwal01 <porwalayush953@gmail.com>
2026-03-07 16:49:18 +0530

    Initial Commit

--- Basics/08 - sets.py
+++ Basics/08 - sets.py
@@ -3,10 +3,10 @@
 a1 = {3, 5, 23, 45, 69, 78}
 a2 = {3, 5, 23}
 
-#a1.clear() #set()
-#a1.pop() #removes random element
-#a1.remove(43) #error raised when given element not found
-#a1.discard(45) #no error raised
+#a1.clear()                   #set()
+#a1.pop()                     #removes random element
+#a1.remove(43)                #error raised when given element not found
+#a1.discard(45)               #no error raised
 
-res = a1.difference(a2)
-print(res) #intersection
+res = a1.difference(a2)       #intersection
+print(res)                    
