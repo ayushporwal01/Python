a4119e881f2ae209274b96f2d2d01962897e07fa
ayushporwal01 <porwalayush953@gmail.com>
2026-03-07 11:14:59 +0530

    Initial Commit

--- Basics/04_strings.py
+++ Basics/04_strings.py
@@ -1,2 +1,10 @@
 name = 'Ayush'
 print(name)
+
+print(name[0:3]) #Ayu
+print(name.upper()) #AYUSH
+print(name.capitalize()) #first character capitalized and rest converted to lowercase
+print(name.count("A")) #1
+print(name.endswith("h")) #True
+print(name.find("h")) #4
+print(name.center(15, "*")) #*****Ayush*****
