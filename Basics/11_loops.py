78f7c9e2d9e9d6635e76939cddce180114454f0d
ayushporwal01 <porwalayush953@gmail.com>
2026-03-08 17:16:54 +0530

    Initial commit

--- Basics/11_loops.py
+++ Basics/11_loops.py
@@ -1,21 +1,21 @@
-# #for loop
+#for loop
 
-# num = int(input("Enter a number: "))
+num = int(input("Enter a number: "))
 
-# for i in range(1, 11):
-#     print(num * i) 
+for i in range(1, 11):
+    print(num * i) 
 
-# a = [1, 34, 456, 34, 234]
+a = [1, 34, 456, 34, 234]
 
-# for item in a:
-#     print(item, end=", ")
+for item in a:
+    print(item, end=", ")
 
 # #while loop
 
-# i = 1
-# while i <= 10:
-#     print(i)
-#     i += 1
+i = 1
+while i <= 10:
+    print(i)
+    i += 1
 
 while(True):
     x = int(input("enter a number: "))
