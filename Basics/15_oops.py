219b52762517b6938c2d9c168d6080c392012624
ayushporwal01 <porwalayush953@gmail.com>
2026-03-09 17:34:24 +0530

    Initial commit

--- Basics/15_oops.py
+++ Basics/15_oops.py
@@ -1,2 +1,3 @@
 #OOPS stands for Object Oriented Programming System.
 #It's a  programming style where we organize code using objects and classes instead of just functions and variables.
+#A class is a blueprint or template for creating objects.
