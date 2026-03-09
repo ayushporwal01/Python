9a5ab11ae687e16fc973f332095ca760777bd025
ayushporwal01 <porwalayush953@gmail.com>
2026-03-09 18:39:11 +0530

    Initial commit

--- Projects/robo-speaker.py
+++ Projects/robo-speaker.py
@@ -3,6 +3,7 @@ from pyttsx3 import speak
 print("Welcome To RoboSpeaker!")
 
 while True:
+    speak("Enter what you want me to speak: ")
     text = input("Enter what you want me speak: ")
 
     if text == "q":
