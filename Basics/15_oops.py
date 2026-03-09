6595d165457ea015467eaac81c77279cf9288fe8
ayushporwal01 <porwalayush953@gmail.com>
2026-03-09 17:49:29 +0530

    Initial commit

--- Basics/15_oops.py
+++ Basics/15_oops.py
@@ -2,3 +2,8 @@
 #It's a  programming style where we organize code using objects and classes instead of just functions and variables.
 #A class is a blueprint or template for creating objects.
 #An object is an instance of a class.
+
+class Employee:
+    salary = 89
+    def getSalary(self):
+        return self.salary
