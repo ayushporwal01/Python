7c8042f42d7cfd02bc46cd2fd062b61bfc981dfb
ayushporwal01 <porwalayush953@gmail.com>
2026-03-09 17:53:21 +0530

    Initial commit

--- Basics/15_oops.py
+++ Basics/15_oops.py
@@ -5,7 +5,8 @@
 
 class Employee:
     def __init__(self, name, salary):
-        self.
+        self.name = name
+        self.salary = salary
 
     def getSalary(self):
         return self.salary
