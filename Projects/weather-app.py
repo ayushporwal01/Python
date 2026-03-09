bad32148745522b18ad44eba3f154904777b9d8f
ayushporwal01 <porwalayush953@gmail.com>
2026-03-09 18:57:48 +0530

    Initial commit

--- Projects/weather-app.py
+++ Projects/weather-app.py
@@ -2,7 +2,7 @@ import requests
 
 city = input("Enter the name of a city: ")
 key = "a43d4b64bede580d5c2ce6baab4e0db3"
-url = f"https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={key}"
+url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={key}&units=metric"
 
 r = requests.get(url)
 print(r.text)
