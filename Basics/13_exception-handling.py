3f987caa85c183ed1c58b72bbab31927d2083a7f
ayushporwal01 <porwalayush953@gmail.com>
2026-03-09 09:06:36 +0530

    Initial commit

--- Basics/13_exception-handling.py
+++ Basics/13_exception-handling.py
@@ -1,1 +1,7 @@
 #exception handling is a way to handle errors that occurs during program execution.
+
+try:
+    a = int(input("Enter a number: "))
+    print(a + 3)
+except Exception as e:
+    print("some error occured: ", e)
