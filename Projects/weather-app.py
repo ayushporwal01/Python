6e1cd62c9dfa4316d3c2bb25751669a64d99ed15
ayushporwal01 <porwalayush953@gmail.com>
2026-03-09 19:14:18 +0530

    Initial commit

--- Projects/weather-app.py
+++ Projects/weather-app.py
@@ -2,6 +2,7 @@ import requests
 
 city = input("Enter the name of a city: ")
 key = "a43d4b64bede580d5c2ce6baab4e0db3"
+
 url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={key}&units=metric"
 
 data = requests.get(url).json()
