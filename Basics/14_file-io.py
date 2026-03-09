e70ca8de49935c8e95901526c5c60e2411168b7a
ayushporwal01 <porwalayush953@gmail.com>
2026-03-09 09:39:06 +0530

    Initial commit

--- Basics/14_file-io.py
+++ Basics/14_file-io.py
@@ -27,6 +27,6 @@ s = "Ayush is a gamer"
 # with open("ayush.txt", "a") as f:
 #     f.write(" and coder")
     
-f = write("ayush.txt", "a")
+f = open("ayush.txt", "a")
 f.write(" and a good human")
 f.close()
