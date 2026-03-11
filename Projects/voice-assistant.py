ee4519781bb7af5a006739113e94d29f8f965615
ayushporwal01 <porwalayush953@gmail.com>
2026-03-11 10:19:31 +0530

    Initial commit

--- Projects/voice-assistant.py
+++ Projects/voice-assistant.py
@@ -3,8 +3,10 @@ import subprocess
 
 recognizer = sr.Recognizer()
 
-with sr.Microphone() as source:
+# Use SoundDevice instead of PyAudio
+with sr.SoundDeviceMicrophone() as source:
     print("Say the app name...")
+    recognizer.adjust_for_ambient_noise(source)  # optional, helps with background noise
     audio = recognizer.listen(source)
 
 try:
@@ -13,13 +15,10 @@ try:
 
     if "chrome" in command:
         subprocess.Popen(["C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"])
-
     elif "notepad" in command:
         subprocess.Popen(["notepad.exe"])
-
     elif "calculator" in command:
         subprocess.Popen(["calc.exe"])
-
     else:
         print("App not recognized")
 
