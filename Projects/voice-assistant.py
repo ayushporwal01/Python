c7f555b5147a30b9ec0926b0618afc9afd1dcaa2
ayushporwal01 <porwalayush953@gmail.com>
2026-03-11 09:15:59 +0530

    Initial commit

--- Projects/voice-assistant.py
+++ Projects/voice-assistant.py
@@ -1,2 +1,11 @@
 import speech_recognition as sr
 import subprocess
+
+recognizer = sr.Recognizer()
+
+with sr.Microphone as source:
+    print("Say the app name: ")
+    audio = recognizer.listen(source)
+
+try:
+    
