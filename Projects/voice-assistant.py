fd3ccd798478de27b142840bd9ed24205d655297
ayushporwal01 <porwalayush953@gmail.com>
2026-03-11 10:12:00 +0530

    Initial commit

--- Projects/voice-assistant.py
+++ Projects/voice-assistant.py
@@ -1,35 +1,25 @@
-import sounddevice as sd
-import numpy as np
 import speech_recognition as sr
 import subprocess
 
-# Settings
-samplerate = 44100  # Sample rate
-duration = 5        # Seconds to listen
-
-print("Listening...")
-
-# Record audio using sounddevice
-audio_data = sd.rec(int(duration * samplerate), samplerate=samplerate, channels=1, dtype='int16')
-sd.wait()  # Wait until recording is finished
-
-# Convert to AudioData for speech_recognition
 recognizer = sr.Recognizer()
-audio_bytes = sr.AudioData(audio_data.tobytes(), samplerate, 2)  # 2 bytes per sample
+
+with sr.Microphone() as source:
+    print("Say the app name...")
+    audio = recognizer.listen(source)
 
 try:
-    command = recognizer.recognize_google(audio_bytes).lower()
+    command = recognizer.recognize_google(audio).lower()
     print("You said:", command)
 
-    # Launch apps based on command
     if "chrome" in command:
-        subprocess.Popen([r"C:\Program Files\Google\Chrome\Application\chrome.exe"])
+        subprocess.Popen(["C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"])
+
     elif "notepad" in command:
         subprocess.Popen(["notepad.exe"])
-    elif "gmail" in command:
-        subprocess.Popen([r"C:\Program Files\Google\Chrome\Application\chrome.exe", "https://mail.google.com"])
-    elif "file explorer" in command:
-        subprocess.Popen(["explorer.exe"])
+
+    elif "calculator" in command:
+        subprocess.Popen(["calc.exe"])
+
     else:
         print("App not recognized")
 
